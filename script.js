66e31fdb9a8f17329f4dfefdc28ab24773bb5564
devmahin <mahinhowlader123@gmail.com>
2024-02-10 22:40:58 +0600

    text-area comment

--- script.js
+++ script.js
@@ -1,15 +1,52 @@
-function blueBtn(){
-    document.body.style.backgroundColor = "blue";
-}
+// function blueBtn(){
+//     document.body.style.backgroundColor = "blue";
+// }
 
-const btnYellow = document.getElementById("btnYellow")
-btnYellow.onclick = yellow;
+// const btnYellow = document.getElementById("btnYellow")
+// btnYellow.onclick = yellow;
 
-function yellow (){
-    document.body.style.backgroundColor = 'yellow';
-}
+// function yellow (){
+//     document.body.style.backgroundColor = 'yellow';
+// }
 
-const green = document.getElementById("green");
-    green.onclick = function (){
-        document.body.style.backgroundColor = "green";
-    }
+// const green = document.getElementById("green");
+//     green.onclick = function (){
+//         document.body.style.backgroundColor = "green";
+//     }
+
+
+
+
+// const welcomeBTN = document.getElementById("welcomeBTN")
+// const change = document.getElementById("change")
+
+// welcomeBTN.addEventListener("click", function (){
+//     change.innerText = "How are you ?"
+// })
+// const welcomeBTN2 = document.getElementById("welcomeBTN2")
+// welcomeBTN2.addEventListener("click", function(){
+//     change.innerText = 'I am fine. and you?'
+// })
+
+
+
+
+// const inputStore = document.querySelector("#inputStore")
+// const inputValue = document.querySelector("#inputValue")
+// const clicks = document.querySelector("#clicks")
+// clicks.addEventListener("click", function(){
+//     let val = inputValue.value;
+//     inputStore.innerText = val;
+//     inputValue.value = "";
+// })
+
+const sectionall = document.querySelector(".sectionall");
+const btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+    let text_area = document.getElementById("text-area")
+    let p = document.createElement("p");
+    p.innerText = text_area.value;
+    sectionall.appendChild(p);
+
+    text_area.value = "";
+})
