6cc375eaf1852bf740a591a7b015e655d60d7000
devmahin <mahinhowlader123@gmail.com>
2024-02-10 19:34:45 +0600

    event basic

---
+++ script.js
@@ -0,0 +1,15 @@
+function blueBtn(){
+    document.body.style.backgroundColor = "blue";
+}
+
+const btnYellow = document.getElementById("btnYellow")
+btnYellow.onclick = yellow;
+
+function yellow (){
+    document.body.style.backgroundColor = 'yellow';
+}
+
+const green = document.getElementById("green");
+    green.onclick = function (){
+        document.body.style.backgroundColor = "green";
+    }
